d1a988041bd8981e18eeac10d77386b919ce681a
ukamsan-sudo <shohsulton040@gmail.com>
2025-04-14 10:49:48 +0300

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -9,8 +9,8 @@ const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('🟢 Bot online\n');
-}).listen(process.env.PORT  3002, () => {
-    console.log(`🟢 Keep-alive server ishga tushdi: http://localhost:${process.env.PORT  3002}`);
+}).listen(process.env.PORT  3003, () => {
+    console.log(`🟢 Keep-alive server ishga tushdi: http://localhost:${process.env.PORT  3003}`);
 });
 
 // --- Sozlamalar ---
